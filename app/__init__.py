62aa0ce25a386355bc3ae52ea1429938dea13cf9
Lakshan-Banneheke <lakshan.18@cse.mrt.ac.lk>
2021-09-09 12:29:16 +0530

    Changed how the app is run and added a .flaskenv

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from app.home import HOME_BP
 from app.binanceStream import BINANCE_BP
+from getStreamData import getStreamData
+
 
 APP = Flask(__name__)
+
+
+@APP.before_first_request
+def activate_job():
+    getStreamData()
+
 
 APP.register_blueprint(HOME_BP, url_prefix='/')
 APP.register_blueprint(BINANCE_BP, url_prefix='/binance/')
