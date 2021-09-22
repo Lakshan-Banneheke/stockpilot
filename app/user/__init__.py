d6a388648ec274c810d0f1b733ec76a046b70670
Lakshan-Banneheke <lakshan.18@cse.mrt.ac.lk>
2021-09-22 16:10:22 +0530

    Login and register api complete

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -4,65 +4,62 @@ import uuid
 from functools import wraps
 
 import jwt
-from flask import Blueprint, render_template, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime, timedelta
 
-import app
-from db_access import db_action, read_one_from_collection
+from db_access import db_action
 
 USER_BP = Blueprint('USER_BP', __name__)
 
 
-#
-# # decorator for verifying the JWT
-# def token_required(f):
-#     @wraps(f)
-#     def decorated(*args, **kwargs):
-#         token = None
-#
-#         if 'x-access-token' in request.headers:
-#             token = request.headers['x-access-token']
-#
-#         if not token:
-#             return jsonify({'message': 'Token is missing !'}), 401
-#
-#         try:
-#             data = jwt.decode(token, os.environ['SECRET_KEY'])
-#             # current_user = get user
-#         except:
-#             return jsonify({'message': 'Token is invalid !'}), 401
-#
-#         return f(current_user, *args, **kwargs)
-#
-#     return decorated
-#
-#
-# @USER_BP.route('/login', methods=['POST'])
-# def login():
-#     auth = json.loads(request.data, strict=False)
-#
-#     if not auth or not auth['email'] or not auth['password']:
-#         return make_response(jsonify({'message': 'All fields are required for logging in'}), 400)
-#
-#     # user = get user from db
-#
-#     if not user:
-#         return make_response(jsonify({'message': 'Wrong Username or Password'}), 401)
-#
-#     if not check_password_hash(user.password, auth['password']):
-#         return make_response(jsonify({'message': 'Wrong Username or Password'}), 401)
-#
-#     else:
-#         try:
-#             token = jwt.encode({
-#                 'public_id': user.public_id,
-#                 'exp': datetime.utcnow() + timedelta(minutes=30)
-#             }, os.environ['SECRET_KEY'])
-#             return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
-#
-#         except:
-#             return make_response(jsonify({'message': 'Login Failed'}), 401)
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=["HS256"])
+            current_user = db_action("read_one", [{"public_id": data['public_id']}, "users"], "admin")
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
+@USER_BP.route('/login', methods=['POST'])
+def login():
+    data = json.loads(request.data, strict=False)
+    user = data['user']
+    if not user or not user['email'] or not user['password']:
+        return make_response(jsonify({'message': 'All fields are required for logging in'}), 400)
+
+    user_data = db_action("read_one", [{"email": user['email']}, "users"], "admin");
+    if not user_data:
+        return make_response(jsonify({'message': 'Wrong Username or Password'}), 401)
+
+    if not check_password_hash(user_data['password'], user['password']):
+        return make_response(jsonify({'message': 'Wrong Username or Password'}), 401)
+
+    else:
+        try:
+            token = jwt.encode({
+                'public_id': user_data['public_id'],
+                'exp': datetime.utcnow() + timedelta(minutes=3000)
+            }, os.environ['SECRET_KEY'], algorithm="HS256")
+            return make_response(jsonify({'token': token}), 201)
+
+        except:
+            return make_response(jsonify({'message': 'Login Failed'}), 401)
 
 
 @USER_BP.route('/register', methods=['POST'])
@@ -71,19 +68,18 @@ def register():
 
     data = json.loads(request.data, strict=False)
     print(data)
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    password = data['password']
+    first_name = data['user']['firstName']
+    last_name = data['user']['lastName']
+    email = data['user']['email']
+    password = data['user']['password']
 
     # if password != repass:
     #     return make_response(jsonify({'message': 'Passwords Do Not Match'}), 400)
 
-    user_email = db_action("read_one", [{"email": email}, "users"], "general")
+    user_email = db_action("read_one", [{"email": email}, "users"], "admin")
 
     if user_email:
-        return make_response(jsonify({'message': 'Email already inuse.'}), 400)
-
+        return make_response(jsonify({'message': 'Email already inuse.'}), 200)
 
     else:
         try:
@@ -95,9 +91,16 @@ def register():
                 "password": generate_password_hash(password, method='sha256'),
             }
 
-            db_action("insert_one", [new_user, "users"], "general")
-            return make_response(jsonify({'message': 'Successfully registered.'}), 201)
-
+            db_action("insert_one", [new_user, "users"], "admin")
+            return make_response(jsonify({'message': 'Successfully Registered.'}), 201)
         except:
-            return make_response(jsonify({'message': 'Registration Failed.'}), 500)
+            return make_response(jsonify({'message': 'Database Error'}), 200)
+
+
+# test route
+@USER_BP.route('/test', methods=['GET'])
+@token_required
+def get_test(current_user):
+    print(current_user)
+    return make_response(jsonify({'message': 'OK'}), 200)
 
