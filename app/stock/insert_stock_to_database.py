5f8950a6e1a44bd22a02947132b9de9c5c923a2b
Lakshan-Banneheke <lakshan.18@cse.mrt.ac.lk>
2021-09-25 22:32:39 +0530

    Stock data inserted to database

--- app/stock/insert_stock_to_database.py
+++ app/stock/insert_stock_to_database.py
@@ -1,3 +1,4 @@
+import json
 import time
 from datetime import timezone, datetime
 
@@ -21,9 +22,63 @@ def insert_1day_to_db(list):
                 ls.append(line)
             a = True
         file.close()
+        json_obj = json.dumps(ls)
         data_obj = {
-            "data_1d": ls
+            "data_1d": json_obj
         }
         db_action("insert_one", [data_obj, i], "admin")
 
-insert_1day_to_db(['aapl'])
+def insert_1hour_to_db(list):
+    for i in list:
+        file_name = i + '.us.txt'
+        path = 'G:\Projects\stockpilot-backend\stock_files\\1_hour\\' + file_name
+        file = open(path, "r")
+        ls = []
+        a = False
+        for x in file:
+            if a:
+                line = x.split(',')
+                datetime_val = line[0] + ' ' + line[1]
+                unix_timestamp = int(datetime.strptime(datetime_val, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc).timestamp()*1000)
+                line = line[1:]
+                line[0] = unix_timestamp
+                line[6] = unix_timestamp
+                ls.append(line)
+            a = True
+        file.close()
+        json_obj = json.dumps(ls)
+        data_obj = {
+            "data_1h": json_obj
+        }
+
+        db_action("insert_one", [data_obj, i], "admin")
+
+def insert_5min_to_db(list):
+    for i in list:
+        file_name = i + '.us.txt'
+        path = 'G:\Projects\stockpilot-backend\stock_files\\5_min\\' + file_name
+        file = open(path, "r")
+        ls = []
+        a = False
+        for x in file:
+            if a:
+                line = x.split(',')
+                datetime_val = line[0] + ' ' + line[1]
+                unix_timestamp = int(datetime.strptime(datetime_val, "%Y-%m-%d %H:%M:%S").replace(tzinfo=timezone.utc).timestamp()*1000)
+                line = line[1:]
+                line[0] = unix_timestamp
+                line[6] = unix_timestamp
+                ls.append(line)
+            a = True
+        file.close()
+        json_obj = json.dumps(ls)
+        data_obj = {
+            "data_5_min": json_obj
+        }
+        db_action("insert_one", [data_obj, i], "admin")
+
+# insert_1day_to_db(['aapl', 'msft', 'goog', 'amzn', 'fb', 'tsla', 'brk-a', 'nvda', 'v', 'jpm', 'jnj', 'baba', 'wmt', 'unh', 'hd'])
+# insert_1hour_to_db(['aapl', 'msft', 'goog', 'amzn', 'fb', 'tsla', 'brk-a', 'nvda', 'v', 'jpm', 'jnj', 'baba', 'wmt', 'unh', 'hd'])
+# insert_5min_to_db(['aapl', 'msft', 'goog', 'amzn', 'fb', 'tsla', 'brk-a', 'nvda', 'v', 'jpm', 'jnj', 'baba', 'wmt', 'unh', 'hd'])
+#
+#
