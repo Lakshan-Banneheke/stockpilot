f3dca83dcf4f3427f1d4ba178fd3bb15c3f30bab
MeelanBandara <64338195+Meelan-98@users.noreply.github.com>
2021-09-25 11:01:24 +0530

    Historical data initiation added; Need to add the logic to feed real time data to the collection as well

--- app/pubsub/data_center.py
+++ app/pubsub/data_center.py
@@ -1,4 +1,7 @@
 from .ps_model import MessageAnnouncer
+from binance.client import Client
+import json
+from db_access import db_action
 
 announcers = {}
 
@@ -16,5 +19,27 @@ def initiate_publisher_set():
     for symbl in symbols:
         announcers[symbl] = {"1d":MessageAnnouncer(),"1h":MessageAnnouncer(),"30m":MessageAnnouncer(),"15m":MessageAnnouncer(),"1m":MessageAnnouncer()}
 
-    
+
+def initiate_historical_data_set():
+
+    for symbl in symbols:
+
+        client = Client()
+
+        data_1m = json.dumps(client.get_historical_klines(symbl, Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC"))
+        data_15m = json.dumps(client.get_historical_klines(symbl, Client.KLINE_INTERVAL_15MINUTE, "1 day ago UTC"))
+        data_30m = json.dumps(client.get_historical_klines(symbl, Client.KLINE_INTERVAL_30MINUTE, "1 day ago UTC"))
+        data_1h = json.dumps(client.get_historical_klines(symbl, Client.KLINE_INTERVAL_1HOUR, "1 day ago UTC"))
+        data_1d = json.dumps(client.get_historical_klines(symbl, Client.KLINE_INTERVAL_1DAY, "1 day ago UTC"))
+
+        db_action("remove_many",[{},symbl],"admin")
+
+        db_action("insert_one",[{"data_1m":data_1m},symbl],"admin")
+        db_action("insert_one",[{"data_15m":data_15m},symbl],"admin")
+        db_action("insert_one",[{"data_30m":data_30m},symbl],"admin")
+        db_action("insert_one",[{"data_1h":data_1h},symbl],"admin")
+        db_action("insert_one",[{"data_1d":data_1d},symbl],"admin")
+
+        print("History Set For:",symbl)
+        
     
--- app/pubsub/ps_model.py
+++ app/pubsub/ps_model.py
@@ -1,3 +1,4 @@
+from db_access import db_action
 import json
 import queue
 
@@ -5,6 +6,7 @@ class MessageAnnouncer:
 
     def __init__(self):
         self.listeners = []
+        self.db_push_queue =[]
 
     def listen(self):
         q = queue.Queue(maxsize=1000)
@@ -12,6 +14,9 @@ class MessageAnnouncer:
         return q
 
     def announce(self, msg):
+
+        deocrated_msg_history = [msg['k']['t'],msg['k']['o'],msg['k']['h'],msg['k']['l'],msg['k']['c'],msg['k']['v'],msg['k']['T'],msg['k']['q'],msg['k']['n'],msg['k']['V'],msg['k']['Q'],msg['k']['B']]
+
         json_msg = json.dumps(msg)
 
         msg = format_sse(data=json_msg)
@@ -21,6 +26,13 @@ class MessageAnnouncer:
                 self.listeners[i].put_nowait(msg)
             except queue.Full:
                 del self.listeners[i]
+        
+        if len(self.db_push_queue)<=10:
+            self.db_push_queue.append(deocrated_msg_history)
+        else:
+            print(self.db_push_queue)
+            hist = db_action("read_one",)
+            self.db_push_queue = []
 
 
 def format_sse(data: str, event=None) -> str:
