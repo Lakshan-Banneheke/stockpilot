c39bf520b99943c76eb78531b8eec960c7fca95e
MeelanBandara <64338195+Meelan-98@users.noreply.github.com>
2021-09-30 13:44:00 +0530

    Notifications history end point finalized

--- app/notifications/__init__.py
+++ app/notifications/__init__.py
@@ -1,6 +1,6 @@
 import flask
 from flask import Blueprint
-from ..pubsub.data_center import listen_notifications
+from ..pubsub.data_center import listen_notifications, historical_nots
 
 NOTIFICATIONS_BP = Blueprint('NOTIFICATIONS_BP', __name__)
 
@@ -16,4 +16,6 @@ def listenn():
     return flask.Response(stream(), mimetype='text/event-stream')
 
 
-
+@NOTIFICATIONS_BP.route('/historical_nots/open_price', methods=['GET'])
+def get_history_nots():
+    return(historical_nots())
