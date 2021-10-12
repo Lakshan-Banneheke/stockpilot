7ca221b039a2d15a567696c0be64584c1cf04875
Lakshan <lakshan.18@cse.mrt.ac.lk>
2021-10-12 21:56:03 +0530

    Refactoring Watchlist

--- app/acessWatchList/__init__.py
+++ app/acessWatchList/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 import json
 
+from app.acessWatchList.services import add_stock_to_watch_list, view_watch_list, remove_from_watch_list
 from app.user import token_required
-from watchList import add_stock_to_watch_list,view_watch_list,remove_from_watch_list
+
 
 WLIST_BP = Blueprint('WLIST_BP', __name__)
 
