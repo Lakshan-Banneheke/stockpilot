be924f2c9ad5ac94abc0ca87b9c0aa2b4af77f68
Lakshan <lakshan.18@cse.mrt.ac.lk>
2021-11-14 14:59:56 +0530

    Small change in ta

--- app/ta/utils.py
+++ app/ta/utils.py
@@ -10,7 +10,7 @@ def get_close_values(type_name, name, interval, s_date):
     else:
         klines = get_historical_stock_data(name, interval, s_date)
     close_prices = np.array([i[4] for i in klines], dtype=float)
-    close_times = [i[6] + 1 for i in klines]
+    close_times = [i[6] for i in klines]
     return close_times, close_prices
 
 
@@ -21,7 +21,7 @@ def get_close_and_volume_values(type_name, name, interval, s_date):
         klines = get_historical_stock_data(name, interval, s_date)
     close_prices = np.array([i[4] for i in klines], dtype=float)
     volume = np.array([i[5] for i in klines], dtype=float)
-    close_times = [i[6] + 1 for i in klines]
+    close_times = [i[6] for i in klines]
     return close_times, volume, close_prices
 
 def get_high_low_close_values(type_name, name, interval, s_date):
@@ -32,5 +32,5 @@ def get_high_low_close_values(type_name, name, interval, s_date):
     high_prices = np.array([i[2] for i in klines], dtype=float)
     low_prices = np.array([i[3] for i in klines], dtype=float)
     close_prices = np.array([i[4] for i in klines], dtype=float)
-    close_times = [i[6] + 1 for i in klines]
+    close_times = [i[6] for i in klines]
     return high_prices, low_prices, close_prices, close_times
